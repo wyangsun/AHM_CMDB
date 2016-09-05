9dcf70972ae7dcc91707f05bd3ceec5209a9ffe4
apple <wyangsun@163.com>
2016-09-05 22:34:07 +0800

    update

--- app.py
+++ app.py
@@ -1,4 +1,36 @@
 import tornado.ioloop
 import tornado.web
-import tornado.options
-I
+from tornado.options import define, options
+
+define("port", default=8000, help="run on the given port", type=int)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+    def post(self, *args, **kwargs):
+        self.write("Post request")
+
+class HostHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("show hosts")
+
+class MapHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("show map")
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        tornado.web.Application.__init__(self, [
+            (r"/host", HostHandler),
+            (r"/map", MapHandler),
+            (r"/", MainHandler),
+                                         ])
+
+def main():
+    tornado.options.parse_command_line()
+    Application().listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
